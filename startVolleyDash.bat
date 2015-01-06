bc1e5e05367860a5f9a5fd51beacedae5e44f837
ghardisty <george.hardisty@gmail.com>
2015-01-05 22:20:24 -0800

    Created bat file to startup VolleyDash
    VolleyDash environment startup

---
+++ startVolleyDash.bat
@@ -0,0 +1,6 @@
+start c:\"Program Files"\nodejs\node c:\webapps\VolleyDash\bin\www
+
+start c:\mongo\mongod --dbpath c:\webapps\VolleyDash\data\
+
+start "Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:3000/matches
+
